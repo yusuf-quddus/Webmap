a7d0314a439a19935d92617393aaf7aa29ac5021
Yusuf Quddus <yusuf_quddus@yahoo.com>
2020-03-31 19:06:55 -0700

    add markers from data file

--- map1.py
+++ map1.py
@@ -1,4 +1,18 @@
 import folium
+import pandas
+
+data = pandas.read_csv("volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My map")
+
+for lt, ln, el in zip(lat, lon, elev): 
+    fg.add_child(folium.Marker(location=[lt, ln], popup = str(el) + " meters", icon = folium.Icon(color='green')))
+
+map.add_child(fg)
+
 map.save("Map1.html")
