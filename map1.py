e8fb522ba9b44155c6c55e312f2b6b2dc71c69c2
Yusuf Quddus <yusuf_quddus@yahoo.com>
2020-03-31 23:57:16 -0700

    add polygons and population color

--- map1.py
+++ map1.py
@@ -14,13 +14,16 @@ def color_producer(elevation):
     else:
         return 'red'
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+map = folium.Map(location=[38.58, -99.09], zoom_start=3, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My map")
 
 for lt, ln, el in zip(lat, lon, elev): 
     fg.add_child(folium.Marker(location=[lt, ln], popup = str(el) + " meters", icon = folium.Icon(color=color_producer(el))))
 
-map.add_child(fg)
+fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding='utf-8-sig').read(), 
+style_function = lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 30000000 else 'orange'
+if 30000000 <= x['properties']['POP2005'] < 90000000 else 'red'}))
 
+map.add_child(fg)
 map.save("Map1.html")
